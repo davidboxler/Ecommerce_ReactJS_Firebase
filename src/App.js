4053a73aeb81076f23c4b36ea738a0678164bb04
davidboxler <davidboxler47@gmail.com>
2024-02-12 23:37:54 -0300

    Se agregan cambios en styles components, se agregan mejoras pwa, dependecias y se agrega global styles

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import { Route, Routes } from "react-router-dom";
 import Spinner from "./components/spinner/spinner";
 import { checkUserSession } from "./store/user/user.action";
 
+import { GlobalStyle } from "./global.styles";
+
 const Authentication = lazy(() => import("./routes/authentication/authentication"));
 const Checkout = lazy(() => import("./routes/checkout/checkout"));
 const Home = lazy(() => import("./routes/home/home"));
 const Navigation = lazy(() => import("./routes/navigation/navigation"));
 const Shop = lazy(() => import("./routes/shop/shop"));
-
 
 const App = () => {
   const dispatch = useDispatch();
@@ -22,6 +23,7 @@ const App = () => {
 
   return (
     <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import { Elements } from "@stripe/react-stripe-js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { Elements } from "@stripe/react-stripe-js";
 import App from "./App";
 import "./index.scss";
-import { store } from "./store/store";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { persistor, store } from "./store/store";
 import { stripePromise } from "./utils/stripe/stripe";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,11 +16,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Elements stripe={stripePromise}>
-          <App />
-        </Elements>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
